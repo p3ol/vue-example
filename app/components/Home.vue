2e64c1df46a37edf41654589f56cf55296afb217
Nicolas AUGER <nicolas@poool.fr>
2020-02-05 15:28:04 +0100

    feat: add router & home view

---
+++ app/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page home">
+    <div class="container">
+      <h1>Home</h1>
+      <p>
+        This is a normal page, without any paywall, with premium &
+        free posts
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    methods: {
+      sendHit() {
+        window.poool('config', {
+          user_is_premium: this.$testUser.premium || false,
+        })
+
+        window.poool('send', 'page-view', 'page')
+      },
+      login() {
+        this.sendHit()
+      }
+    },
+    mounted() {
+      this.sendHit()
+    },
+    beforeRouteLeave(to, from, next) {
+      window.poool('flush')
+      next()
+    }
+  }
+</script>
--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import { routes } from './routes';
 
 import App from './components/App.vue';
 
 Vue.use(VueRouter);
 
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+});
 
 Vue.prototype.$testUser = {
   logged: false,
@@ -13,5 +18,6 @@ Vue.prototype.$testUser = {
 };
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#root');
